25e4f243633bf30c2aa690214f1cfc42c2b96680
ShubhamKumar37 <sk9818281820@gmail.com>
2024-03-07 00:09:22 +0530

    first commit react

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NewNav from './Component/NewNav'
+
+function App() {
+  return (
+    <div>
+      <NewNav Title="Shubham_Company" About="New About"></NewNav>
+    </div>
+  );
+}
+
+export default App;
