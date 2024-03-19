bdb76ab48b5b992273c2b66ca7f905957b38beef
ShubhamKumar37 <sk9818281820@gmail.com>
2024-03-19 22:42:29 +0530

    typed gidf completed custom hook left

--- gif-project-starter/src/components/RandomGIF.jsx
+++ gif-project-starter/src/components/RandomGIF.jsx
@@ -1,20 +1,24 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import Loader from "./Loader";
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 export default function RandomGIF() {
   const URL = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
   const [GIF, setGIF] = useState("");
+  const [Spinner, SetSpinner] = useState(false);
 
   async function fetchGIF() {
     try {
-      const {data} = await axios.get(URL);
-      // console.log(response);
+      SetSpinner(true);
+      const { data } = await axios.get(URL);
       const gifURL = data.data.images.downsized_large.url;
       console.log(gifURL);
       setGIF(gifURL);
-    } catch (error) {
+      SetSpinner(false);
+    }
+    catch (error) {
       console.error("Error fetching GIF:", error);
     }
   }
@@ -30,7 +34,7 @@ export default function RandomGIF() {
   return (
     <div className="w-8/12 relative flex flex-col items-center bg-green-300 rounded-lg border border-black min-h-[10rem] justify-between p-2 text-xl font-bold underline text-[#415c3d] gap-3">
       <h1>A Random GIF</h1>
-      <img src={GIF} width="450" alt="Random GIF" className="rounded-lg"></img>
+      {Spinner === true ? <Loader></Loader> : <img src={GIF} width="450" alt="Random GIF" className="rounded-lg"></img>}
 
       <button onClick={generateHandler} className="bg-blue-800 text-white font-bold text-xl w-7/12 rounded-lg hover:scale-110 transition-all duration-150 p-1">Generate a GIF</button>
     </div>
--- gif-project-starter/src/components/TypedGIF.jsx
+++ gif-project-starter/src/components/TypedGIF.jsx
@@ -1,5 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import Loader from "./Loader";
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
 export default function TypedGIF() {
+  const [Type, SetType] = useState('Nigga');
+  const URL = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${Type}`;
+  const [GIF, setGIF] = useState("");
+  const [Spinner, SetSpinner] = useState(false);
+
+  async function fetchGIF() {
+    try {
+      SetSpinner(true);
+      const { data } = await axios.get(URL);
+      const gifURL = data.data.images.downsized_large.url;
+      console.log(gifURL);
+      setGIF(gifURL);
+      SetSpinner(false);
+    }
+    catch (error) {
+      console.error("Error fetching GIF:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchGIF();
+  }, []);
+
+  function generateHandler() {
+    fetchGIF();
+  }
+
   return (
-    <div>Typed GIF</div>
+    <div className="w-8/12 relative flex flex-col items-center bg-green-300 rounded-lg border border-black min-h-[10rem] justify-between p-2 text-xl font-bold underline text-[#415c3d] gap-3">
+      <h1>A {Type} GIF</h1>
+      {Spinner === true ? <Loader></Loader> : <img src={GIF} width="450" alt="Random GIF" className="rounded-lg"></img>}
+
+      <input type="text" className="relative text-center px-[4rem] rounded-lg no-underline" onChange={(Event) => {SetType(Event.target.value)}} value={Type}></input>
+
+      <button onClick={generateHandler} className="bg-blue-800 text-white font-bold text-xl w-7/12 rounded-lg hover:scale-110 transition-all duration-150 p-1">Generate {Type} GIF</button>
+    </div>
   );
 }
